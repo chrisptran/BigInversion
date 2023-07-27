bb5156e8a6d0c2572ba4c17f653957af19415aa5
chrisptran <christopher.p.tran@gmail.com>
2023-07-26 23:20:47 -0700

    Second commit

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
+
+        const [count, setCount] = useState(props.age);
+
+        const handleClick = () => {
+            setCount(
+                count + 1
+            );
+        }
+    
+
     return (
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {count}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={handleClick}>Birthday: {props.firstName}</button>
         </div>
-    )
+    );
 }
 
-export default PersonCard
+export default PersonCard;
